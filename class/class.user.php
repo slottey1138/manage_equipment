dece1b7e4bf5b75e5c58478f19931df27db1d495
SlotteyWork <Slottey0@gmail.com>
2018-08-30 11:26:44 +0700

    commit v4

--- class/class.user.php
+++ class/class.user.php
@@ -1,82 +1,94 @@
 <?php
-require_once("config.php");
+
+require_once('config.php');
 
 class USER
-{
-    private $db;
- 
-    function __construct()
-    {
-        $this->db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+{	
+
+	private $conn;
+	
+	public function __construct()
+	{
+		$database = new Database();
+		$db = $database->Connection();
+		$this->conn = $db;
     }
- 
-    public function register($mb_id,$mb_fname,$mb_lname,$mb_pass)
-    {
-       try
-       {
-           $new_password = password_hash($mb_pass, PASSWORD_DEFAULT);
-   
-           $stmt = $this->db->prepare("INSERT INTO tbl_member(mb_id,mb_fname,mb_lname,mb_pass) 
-                                                       VALUES(:mb_id, :mb_fname, :mb_lname, :mb_pass)");
-              
-           $stmt->bindparam(":mb_id", $mb_id);
-           $stmt->bindparam(":mb_fname", $mb_fname);
-           $stmt->bindparam(":mb_lname", $mb_lname);
-           $stmt->bindparam(":mb_pass", $mb_pass);     
-           $stmt->execute(); 
-   
-           return $stmt; 
-       }
-       catch(PDOException $e)
-       {
-           echo $e->getMessage();
-       }    
-    }
- 
-    public function login($mb_id,$mb_pass)
-    {
-       try
-       {
-          $stmt = $this->db->prepare("SELECT * FROM tbl_member WHERE mb_id=:mb_id");
-          $stmt->execute(array(':mb_id'=>$mb_id));
-          $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-          if($stmt->rowCount() > 0)
-          {
-             if(password_verify($mb_pass, $userRow['mb_pass']))
-             {
-                $_SESSION['user_session'] = $userRow['user_id'];
-                return 1;
-             }
-             else
-             {
-                return 2;
-             }
-          }
-       }
-       catch(PDOException $e)
-       {
-           echo $e->getMessage();
-       }
-   }
- 
-   public function is_loggedin()
-   {
-      if(isset($_SESSION['user_session']))
-      {
-         return true;
-      }
-   }
- 
-   public function redirect($url)
-   {
-       header("Location: $url");
-   }
- 
-   public function logout()
-   {
-        session_destroy();
-        unset($_SESSION['user_session']);
-        return true;
-   }
+	
+	public function runQuery($sql)
+	{
+		$stmt = $this->conn->prepare($sql);
+		return $stmt;
+	}
+	
+	public function register($mb_id,$mb_fname,$mb_lname,$mb_pass)
+	{
+		try
+		{
+			$new_password = password_hash($mb_pass, PASSWORD_DEFAULT);
+			
+			$stmt = $this->conn->prepare("INSERT INTO tbl_member(mb_id,mb_fname,mb_lname,mb_pass) 
+		                                               VALUES(:mb_id, :mb_fname,:mb_lname, :mb_pass)");
+				
+			$stmt->bindparam(":mb_id", $mb_id);
+			$stmt->bindparam(":mb_fname", $mb_fname);
+			$stmt->bindparam(":mb_lname", $mb_lname);
+			$stmt->bindparam(":mb_pass", $new_password);										  
+				
+			$stmt->execute();	
+			
+			return $stmt;	
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}				
+	}
+	
+	
+	public function doLogin($mb_id,$mb_pass)
+	{
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT mb_id, mb_pass FROM tbl_member WHERE mb_id =:mb_id");
+			$stmt->execute(array(':mb_id'=>$mb_id));
+			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+			if($stmt->rowCount() == 1)
+			{
+				if($mb_pass == $userRow['mb_pass'])
+				{
+					$_SESSION['user_session'] = $userRow['mb_id'];
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+	
+	public function is_loggedin()
+	{
+		if(isset($_SESSION['user_session']))
+		{
+			return true;
+		}
+	}
+	
+	public function redirect($url)
+	{
+		header("Location: $url");
+	}
+	
+	public function doLogout()
+	{
+		session_destroy();
+		unset($_SESSION['user_session']);
+		return true;
+	}
 }
 ?>
--- class/config.php
+++ class/config.php
@@ -1,7 +1,28 @@
 <?php
+class Database
+{   
+    private $host = "localhost";
+    private $db_name = "manage_equipment";
+    private $username = "root";
+    private $password = "12345678";
+    public $conn;
+     
+    public function Connection()
+	{
+     
+	    $this->conn = null;    
+        try
+		{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+        }
+		catch(PDOException $exception)
+		{
+            echo "Connection error: " . $exception->getMessage();
+        }
+         
+        return $this->conn;
+    }
+}
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '12345678');
-define('DB_DATABASE', 'manage_equipment');
 ?>
